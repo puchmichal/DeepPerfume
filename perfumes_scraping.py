c81f294d6a27917fe8da272c80ab0fe42c05ffb1
unknown <alicja.kocieniewska@op.pl>
2019-04-27 18:44:01 +0200

    fragrance group

--- perfumes_scraping.py
+++ perfumes_scraping.py
@@ -4,16 +4,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def generate_url_page(page_number):
-  base_url = "https://www.iperfumy.pl/perfumy/"
+    base_url = "https://www.iperfumy.pl/perfumy/"
 
-  if page_number == 1:
-    return base_url
+    if page_number == 1:
+        return base_url
   
-  current_url = base_url + "?f=" + str(page_number) + "-1-6362"
-  return current_url
+    current_url = base_url + "?f=" + str(page_number) + "-1-6362"
+    return current_url
 
-scrapped_data = pd.DataFrame({"brand": [], "product_name": [], "image_link": [],
-                              "description": [], "list_description": [],
+scrapped_data = pd.DataFrame({"brand": [], "product_name": [], "fragrance_group": [],
+                              "image_link": [],"description": [], "list_description": [],
                               "price": []})
 
 page_number = 1
@@ -55,11 +55,20 @@ def scrap_link(link):
           print("nie ma punktów" + " " + link)
     except:
         print("nie ma opisu" +" " + link)
+        
+    fragrance_group = ""
+    
+    try:
+        fragrance_information = soup.find_all("div", {"id": "pdCharacteristics"})[0]
+        fragrance_group = fragrance_information.find_all("dd")[-1].text
+    except:
+        print("nie ma grupy zapachowej" + " " + link)
     
     image = soup.find_all("img", {"id": "pd-image-main"})[0]["src"]
     
     scrapped_data.loc[scrapped_data.shape[0], :] = [brand_name, product_name, 
-                     image, main_description, list_description, price]
+                     fragrance_group, image, main_description, list_description, 
+                     price]
 
 
 while True:
@@ -83,5 +92,3 @@ while True:
 
     page_number += 1
 pbar.close()
-
-
