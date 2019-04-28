9fad48672019f1d19652406571fd8b811300e740
unknown <alicja.kocieniewska@op.pl>
2019-04-28 20:59:10 +0200

    cleaning and increasing readability

--- perfumes_scraping.py
+++ perfumes_scraping.py
@@ -11,7 +11,7 @@ def generate_url_page(page_no):
 
     if page_no == 1:
         return base_url
-
+    # url pattern for each page except first
     url = base_url + "?f=" + str(page_no) + "-1-6362"
     return url
 
@@ -22,71 +22,75 @@ def scrap_link(link):
     perfume_page = urlopen(link)
 
     perfume_soup = BeautifulSoup(perfume_page, 'html.parser')
-    
-    brand_name = ""
-    product_name = ""
-    
+        
     try:
         brand_and_product = perfume_soup.find_all("h1", {"itemprop": "name"})[0].find_all("span")
         
         try:
             brand_name = brand_and_product[0].text
         except: 
-            print("nie ma brandu" + " " + link)
+            # if no brand name avaliable, empty string will be added
+            brand_name = ""
+            print("nie ma brandu w {}".format(link))
         
         try:
             product_name = brand_and_product[1].text
         except:
-            print("nie ma nazwy produktu" + " " + link)
+            # if no product name avaliable, empty string will be added
+            product_name = ""
+            print("nie ma nazwy produktu w {}".format(link))
     except:
-        print("nie ma żadnej nazwy" + " " + link)
+        print("nie ma żadnej nazwy w {}".format(link))
         
     price = perfume_soup.find_all("div", {"id": "pd-price"})[0].find_all("span")[0].text
-
+    
+    # in case of missing information, empty strings will be added
     main_description = ""
-    list_description = ""
-
+    bullet_description = ""
+    
+    # caching all possible exceptions (missing brand, product or both)
     try:
         description = perfume_soup.find_all("div", {"id": "pd-description-text"})[0]
 
         try:
+            # adding each paragraph to the descripton
             for paragraph in description.find_all("p"):
                 main_description += paragraph.text + " "
-            main_description = main_description[:-1]
         except:
-            print("nie ma description" + " " + link)
+            print("nie ma description w {}".format(link))
 
         try:
+            # adding each bullet point to the descripton
             for bullet_point in description.find_all("li"):
-                list_description += bullet_point.text + " "
-            list_description = list_description[:-1]
+                bullet_description += bullet_point.text + " "
         except:
-            print("nie ma punktów" + " " + link)
+            print("nie ma punktów w {}".format(link))
     except:
-        print("nie ma opisu" + " " + link)
-
-    fragrance_group = ""
+        print("nie ma opisu w {}".format(link))
 
     try:
         fragrance_information = perfume_soup.find_all("div", {"id": "pdCharacteristics"})[0]
         fragrance_group = fragrance_information.find_all("dd")[-1].text
     except:
-        print("nie ma grupy zapachowej" + " " + link)
+        # if no fragrance group avaliable, empty string will be added
+        fragrance_group = ""
+        print("nie ma grupy zapachowej w {}".format(link))
 
     image = perfume_soup.find_all("img", {"id": "pd-image-main"})[0]["src"]
-
+    
+    # appending scrapped data to the data frame
     scrapped_data.loc[scrapped_data.shape[0], :] = [brand_name, product_name,
                                                     fragrance_group, image, main_description,
-                                                    list_description,
+                                                    bullet_description,
                                                     price]
 
 
 # start scrapping
 page_number = 1
-loading_bar = tqdm.tqdm(total=439*24)
+elements_on_page = 24
 
 scrapped_data = pd.DataFrame({"brand": [], "product_name": [], "fragrance_group": [],
-                              "image_link": [], "description": [], "list_description": [],
+                              "image_link": [], "description": [], "bullet_description": [],
                               "price": []})
 
 # looping over all pages with perfumes, each page 24 perfumes
@@ -100,12 +104,15 @@ while True:
 
     # finding all links to perfume's pages
     links_on_current_page = soup.find_all('a', {"class": "spc"})
-    links = [str(link['href']) for link in links_on_current_page]
-
+    
+    # extracting all the links and converting them to the list
+    links = list(map(lambda link: str(link['href']), links_on_current_page))
+    
     if page_number == 1:
         next_pages_links = soup.find("span", {"class": "pages"})
         number_of_pages = int(next_pages_links.find_all("a")[-2].text)
-        loading_bar = tqdm.tqdm(total=number_of_pages*24, smoothing=0)
+        loading_bar = tqdm.tqdm(total=number_of_pages*elements_on_page, 
+                                smoothing=0)
 
     # scrapping all information about each perfume
     for link in links:
